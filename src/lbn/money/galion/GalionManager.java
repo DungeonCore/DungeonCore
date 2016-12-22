22fc883a3d2fc11888f95ef7b15d307550b313a9
azure_crystal_ <stillcrystal@outlook.jp>
2016-12-23 06:08:12 +0900

    経済システム内部の変更
    Map本体の同期化を行いました。
    ログに出力するメソッドの可読性の向上

--- src/lbn/money/galion/GalionManager.java
+++ src/lbn/money/galion/GalionManager.java
@@ -1,6 +1,8 @@
 package lbn.money.galion;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -10,61 +12,54 @@ import lbn.common.other.SystemLog;
 import lbn.player.playerIO.PlayerIODataManager;
 
 public class GalionManager {
-	static HashMap<Player, Integer> galionMap = new HashMap<Player, Integer>();
-
-	public static void setGalion(Player p, int val, GalionEditReason reason) {
-		remove(p);
-		addGalion(p, val, GalionEditReason.system);
-	}
-	public static void addGalion(Player p, int val, GalionEditReason reason) {
-		if (!galionMap.containsKey(p)) {
-			galionMap.put(p, 0);
-		}
-
-		PlayerChangeGalionsEvent event = new PlayerChangeGalionsEvent(p, val, reason);
-		Bukkit.getServer().getPluginManager().callEvent(event);
-
-		int money = galionMap.get(p);
-		money += event.getGalions();
-
-		money = Math.max(money, 0);
-
-		galionMap.put(p, money);
-
-		//SQLに挿入
-//		PlayerDataSql playerDataSql = new PlayerDataSql(p);
-//		playerDataSql.setGalions(val);
-
-		printLog(p, val, reason);
-
-		PlayerIODataManager.save(p);
-	}
-
-	public static int getGalion(Player p) {
-		if (!galionMap.containsKey(p)) {
-			galionMap.put(p, 0);
-		}
-		return galionMap.get(p);
-	}
-
-	public static void remove(Player p) {
-
-		PlayerIODataManager.save(p);
-//		galionMap.remove(p);
-	}
-
-	public static void printLog(Player p, int val, GalionEditReason reason) {
-		if (reason != GalionEditReason.mob_drop) {
-			StringBuilder stringBuilder = new StringBuilder();
-			stringBuilder.append(p.getName());
-			stringBuilder.append(" get ");
-			stringBuilder.append(val);
-			stringBuilder.append("galions by ");
-			stringBuilder.append(reason);
-			stringBuilder.append(", (total ");
-			stringBuilder.append(galionMap.get(p));
-			stringBuilder.append(" galions)");
-			SystemLog.addLog(stringBuilder.toString());
-		}
-	}
+  
+  // Galion を複数スレッドから排他制御する場合に必要
+  private static Map<Player, Integer> galionMap = Collections.synchronizedMap(new HashMap<Player, Integer>());
+  
+  public static void setGalion(Player p, int val, GalionEditReason reason) {
+    remove(p);
+    addGalion(p, val, GalionEditReason.system);
+  }
+  
+  public static void addGalion(Player p, int val, GalionEditReason reason) {
+    if (!galionMap.containsKey(p)) {
+      galionMap.put(p, 0);
+    }
+    
+    PlayerChangeGalionsEvent event = new PlayerChangeGalionsEvent(p, val, reason);
+    Bukkit.getServer().getPluginManager().callEvent(event);
+    
+    int money = galionMap.get(p);
+    money += event.getGalions();
+    money = Math.max(money, 0);
+    galionMap.put(p, money);
+    
+    // SQLに挿入
+    // PlayerDataSql playerDataSql = new PlayerDataSql(p);
+    // playerDataSql.setGalions(val);
+    
+    printLog(p, val, reason);
+    
+    PlayerIODataManager.save(p);
+  }
+  
+  public static int getGalion(Player p) {
+    if (!galionMap.containsKey(p)) {
+      galionMap.put(p, 0);
+    }
+    return galionMap.get(p);
+  }
+  
+  public static void remove(Player p) {
+    PlayerIODataManager.save(p);
+    // galionMap.remove(p);
+  }
+  
+  public static void printLog(Player p, int val, GalionEditReason reason) {
+    if (reason != GalionEditReason.mob_drop) {
+      final String format = "%s get galions by %s, (total %d galions)";
+      final String message = String.format(format, p.getName(), val, reason.toString(), galionMap.get(p));
+      SystemLog.addLog(message);
+    }
+  }
 }
