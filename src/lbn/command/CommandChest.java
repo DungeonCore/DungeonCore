a145ee09383d0ed6435fdc16145127711128aa50
azure_crystal_ <stillcrystal@outlook.jp>
2017-01-01 00:33:33 +0900

    switchを使うように変更(多少見やすく)

--- src/lbn/command/CommandChest.java
+++ src/lbn/command/CommandChest.java
@@ -1,9 +1,8 @@
 package lbn.command;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
-import net.md_5.bungee.api.ChatColor;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -30,8 +29,9 @@ import lbn.chest.SpletSheetChest;
 import lbn.chest.wireless.RepositoryType;
 import lbn.dungeoncore.SpletSheet.ChestSheetRunable;
 import lbn.dungeoncore.SpletSheet.SpletSheetExecutor;
+import net.md_5.bungee.api.ChatColor;
 
-public class CommandChest implements CommandExecutor, TabCompleter{
+public class CommandChest implements CommandExecutor, TabCompleter {
 
 	public static HashMap<Player, Location> chestClickMap = new HashMap<Player, Location>();
 
@@ -45,12 +45,21 @@ public class CommandChest implements CommandExecutor, TabCompleter{
 		}
 
 		boolean result = false;
-		if (params.length >= 1 && params[0].equalsIgnoreCase("reload")) {
-			result = reload(paramCommandSender, params, chestSheetRunable);
-		} else if (params.length == 1 && params[0].equalsIgnoreCase("delete")) {
-			result = delete(paramCommandSender, chestSheetRunable);
-		} else if (params.length == 2 && params[0].equalsIgnoreCase("souko")) {
-			result = addRepository(paramCommandSender, params, chestSheetRunable);
+		if (params.length >= 1) {
+			switch (params[0].toLowerCase()) {
+			case "reload":
+				result = reload(paramCommandSender, params, chestSheetRunable);
+				break;
+			case "delete":
+				result = delete(paramCommandSender, chestSheetRunable);
+				break;
+			case "souko":
+				if (params.length >= 2)
+					result = addRepository(paramCommandSender, params, chestSheetRunable);
+				else
+					result = add(paramCommandSender, params, chestSheetRunable);
+				break;
+			}
 		} else {
 			result = add(paramCommandSender, params, chestSheetRunable);
 		}
@@ -80,7 +89,7 @@ public class CommandChest implements CommandExecutor, TabCompleter{
 		Chest c = (Chest) data;
 		BlockFace facing = c.getFacing();
 		Block relative = chestLoc.getBlock().getRelative(facing);
-//		BlockFace oppositeFace = facing.getOppositeFace();
+		//		BlockFace oppositeFace = facing.getOppositeFace();
 		relative.setType(Material.WALL_SIGN);
 		Sign sign = (Sign) relative.getState();
 		sign.setLine(0, "[chest]");
@@ -104,14 +113,14 @@ public class CommandChest implements CommandExecutor, TabCompleter{
 	protected boolean delete(CommandSender paramCommandSender, ChestSheetRunable chestSheetRunable) {
 		paramCommandSender.sendMessage("現在この機能は使えません。");
 		boolean result;
-//		Player p = (Player) paramCommandSender;
-//		Location chestLoc = getChestLoc(p);
-//		if (chestLoc == null) {
-//			p.sendMessage("削除するchestを先にクリックして下さい");
-//		}
-//		CustomChestManager.removeChest(chestLoc);
-//		chestSheetRunable.deleteData("chest_location=" + ChestSheetRunable.getLocationString(chestLoc));
-//		SpletSheetExecutor.onExecute(chestSheetRunable);
+		//		Player p = (Player) paramCommandSender;
+		//		Location chestLoc = getChestLoc(p);
+		//		if (chestLoc == null) {
+		//			p.sendMessage("削除するchestを先にクリックして下さい");
+		//		}
+		//		CustomChestManager.removeChest(chestLoc);
+		//		chestSheetRunable.deleteData("chest_location=" + ChestSheetRunable.getLocationString(chestLoc));
+		//		SpletSheetExecutor.onExecute(chestSheetRunable);
 		result = true;
 		return result;
 	}
@@ -128,43 +137,43 @@ public class CommandChest implements CommandExecutor, TabCompleter{
 		double moveX = 0;
 		double moveY = 0;
 		double moveZ = 0;
-//
-//		if (params.length < 3) {
-//			return false;
-//		}
-//
-//		if (params.length == 8 || params.length == 9) {
-//			paramCommandSender.sendMessage("チェストを開けた後の移動先の座標が足りません。x y z全て必要です");
-//			return true;
-//		}
-//
-//		try {
-//			switch (params.length) {
-//			case 10:
-//				moveX = Double.parseDouble(params[9]);
-//			case 9:
-//				moveY = Double.parseDouble(params[8]);
-//			case 8:
-//				moveZ = Double.parseDouble(params[7]);
-//			case 7:
-//				maxItemCount = Integer.parseInt(params[6]);
-//			case 6:
-//				minItemCount = Integer.parseInt(params[5]);
-//			case 5:
-//				allPlayerSameFlg = Boolean.parseBoolean(params[4]);
-//			case 4:
-//				refuelTime = Double.parseDouble(params[3]);
-//			case 3:
-//				x = Double.parseDouble(params[0]);
-//				y = Double.parseDouble(params[1]);
-//				z = Double.parseDouble(params[2]);
-//			default:
-//				break;
-//			}
-//		} catch (Exception e) {
-//			paramCommandSender.sendMessage("入力したデータに誤りがあります。");
-//			return true;
-//		}
+		//
+		//		if (params.length < 3) {
+		//			return false;
+		//		}
+		//
+		//		if (params.length == 8 || params.length == 9) {
+		//			paramCommandSender.sendMessage("チェストを開けた後の移動先の座標が足りません。x y z全て必要です");
+		//			return true;
+		//		}
+		//
+		//		try {
+		//			switch (params.length) {
+		//			case 10:
+		//				moveX = Double.parseDouble(params[9]);
+		//			case 9:
+		//				moveY = Double.parseDouble(params[8]);
+		//			case 8:
+		//				moveZ = Double.parseDouble(params[7]);
+		//			case 7:
+		//				maxItemCount = Integer.parseInt(params[6]);
+		//			case 6:
+		//				minItemCount = Integer.parseInt(params[5]);
+		//			case 5:
+		//				allPlayerSameFlg = Boolean.parseBoolean(params[4]);
+		//			case 4:
+		//				refuelTime = Double.parseDouble(params[3]);
+		//			case 3:
+		//				x = Double.parseDouble(params[0]);
+		//				y = Double.parseDouble(params[1]);
+		//				z = Double.parseDouble(params[2]);
+		//			default:
+		//				break;
+		//			}
+		//		} catch (Exception e) {
+		//			paramCommandSender.sendMessage("入力したデータに誤りがあります。");
+		//			return true;
+		//		}
 		Player p = (Player) paramCommandSender;
 
 		Location contentsLoc = null;
@@ -198,23 +207,20 @@ public class CommandChest implements CommandExecutor, TabCompleter{
 			if (allPlayerSameFlg) {
 				chest = new AllPlayerSameContentChest(chestLoc, contentsLoc, (int) (refuelTime * 20), moveLoc, minItemCount, maxItemCount, 10, true);
 			} else {
-				chest = new EachPlayerContentChest(chestLoc, contentsLoc, (int) (refuelTime * 20), moveLoc, minItemCount, maxItemCount,  10, true);
+				chest = new EachPlayerContentChest(chestLoc, contentsLoc, (int) (refuelTime * 20), moveLoc, minItemCount, maxItemCount, 10, true);
 			}
 			CustomChestManager.registChest(chestLoc, chest);
 
 			//バグるのでなし
-//		chestSheetRunable.deleteData("chestlocation=" + ChestSheetRunable.getLocationString(chestLoc));
-			chestSheetRunable.addData(ChestSheetRunable.createDataMap(chestLoc, contentsLoc, refuelTime, allPlayerSameFlg, moveLoc, minItemCount, maxItemCount));
+			//		chestSheetRunable.deleteData("chestlocation=" + ChestSheetRunable.getLocationString(chestLoc));
+			chestSheetRunable.addData(
+					ChestSheetRunable.createDataMap(chestLoc, contentsLoc, refuelTime, allPlayerSameFlg, moveLoc, minItemCount, maxItemCount));
 
 		}
 		SpletSheetExecutor.onExecute(chestSheetRunable);
 
-
-
 		return true;
 	}
-
-
 
 	protected boolean reload(CommandSender paramCommandSender, String[] params, ChestSheetRunable chestSheetRunable) {
 		chestSheetRunable.getData(null);
@@ -229,7 +235,8 @@ public class CommandChest implements CommandExecutor, TabCompleter{
 	@Override
 	public List<String> onTabComplete(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
 		if (arg3.length == 1) {
-			return (List<String>)StringUtil.copyPartialMatches(arg3[0], ChestLocationManager.getNames(), new ArrayList<String>(ChestLocationManager.getNames().size()));
+			return (List<String>) StringUtil.copyPartialMatches(arg3[0], ChestLocationManager.getNames(),
+					new ArrayList<String>(ChestLocationManager.getNames().size()));
 		}
 		return ImmutableList.of();
 	}
