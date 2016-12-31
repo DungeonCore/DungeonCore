50a9f68060eba95d9be0d6f0f43b8c2c6bb6b9a9
azure_crystal_ <stillcrystal@outlook.jp>
2017-01-01 00:42:59 +0900

    Added Javadoc

--- src/lbn/Announce.java
+++ src/lbn/Announce.java
@@ -4,17 +4,35 @@ import static java.lang.String.*;
 import static org.bukkit.Bukkit.*;
 import static org.bukkit.ChatColor.*;
 
+/**
+ * This class has utility methods for message broadcasting.
+ */
 public final class Announce {
 
+	/**
+	 * Format string for announcement
+	 */
 	private static final String FORMAT_ANNOUNCE = format("%s[%sAnnounce%s]:%s%%s", DARK_GRAY, GREEN, DARK_GRAY, AQUA);
+
+	/**
+	 * Format string for attention
+	 */
 	private static final String FORMAT_ATTENTION = format("%s[%sAttention!%s]:%s%%s", DARK_GRAY, DARK_RED, DARK_GRAY, WHITE);
 
-	public static void announce(String msg) {
-		broadcastMessage(format(FORMAT_ANNOUNCE, msg));
+	/**
+	 * Broadcast messages with "Announce" prefixes.
+	 * @param message Message for broadcasting
+	 */
+	public static void announce(String message) {
+		broadcastMessage(format(FORMAT_ANNOUNCE, message));
 	}
 
-	public static void attention(String msg) {
-		broadcastMessage(format(FORMAT_ATTENTION, msg));
+	/**
+	 * Broadcast messages with "Attention" prefixes.
+	 * @param message Message for broadcasting
+	 */
+	public static void attention(String message) {
+		broadcastMessage(format(FORMAT_ATTENTION, message));
 	}
 
 }
