a701e894634e998fa4d339b03b39cd9adeca52eb
azure_crystal_ <stillcrystal@outlook.jp>
2017-01-01 00:56:05 +0900

    modified variable names

--- src/lbn/command/MoneyCommand.java
+++ src/lbn/command/MoneyCommand.java
@@ -12,12 +12,12 @@ import lbn.money.galion.GalionEditReason;
 import lbn.money.galion.GalionManager;
 import lbn.util.Message;
 
-public class MoneyCommand implements CommandExecutor{
+public class MoneyCommand implements CommandExecutor {
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		Player p = (Player) arg0;
-		if (arg3.length == 0) {
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+		Player p = (Player) sender;
+		if (args.length == 0) {
 			int galion = GalionManager.getGalion(p);
 			Message.sendMessage(p, ChatColor.GREEN + "You have {0} Galions", galion);
 			return true;
@@ -27,31 +27,31 @@ public class MoneyCommand implements CommandExecutor{
 			return true;
 		}
 
-		if (arg3.length == 1) {
-			Player playerExact = Bukkit.getPlayerExact(arg3[0]);
-			if (playerExact != null) {
-				int galion = GalionManager.getGalion(playerExact);
-				arg0.sendMessage(playerExact.getDisplayName() + " have " + galion + " galions");
+		if (args.length == 1) {
+			Player target = Bukkit.getPlayerExact(args[0]);
+			if (target != null) {
+				int galion = GalionManager.getGalion(target);
+				sender.sendMessage(target.getDisplayName() + " have " + galion + " galions");
 			} else {
-				arg0.sendMessage(arg3[0] + "というプレイヤーは存在しません。");
+				sender.sendMessage(args[0] + "というプレイヤーは存在しません。");
 			}
 			return true;
 		}
 
-		if (arg3.length < 2) {
+		if (args.length < 2) {
 			return false;
 		}
 		int galions = 0;
 		try {
-			galions = Integer.parseInt(arg3[1]);
-		}catch (NumberFormatException e) {
+			galions = Integer.parseInt(args[1]);
+		} catch (NumberFormatException e) {
 			p.sendMessage("金額が不正です");
 			return false;
 		}
 
 		Player target = p;
-		if (arg3.length == 3) {
-			Player player = Bukkit.getPlayer(arg3[2]);
+		if (args.length == 3) {
+			Player player = Bukkit.getPlayer(args[2]);
 			if (player == null || !player.isOnline()) {
 				p.sendMessage("Player名が不正です");
 				return false;
@@ -59,8 +59,7 @@ public class MoneyCommand implements CommandExecutor{
 			p = player;
 		}
 
-
-		String operate = arg3[0];
+		String operate = args[0];
 		if (operate.equalsIgnoreCase("set")) {
 			GalionManager.setGalion(target, galions, GalionEditReason.command);
 		} else if (operate.equalsIgnoreCase("add")) {
