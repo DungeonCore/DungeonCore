741b342e8db8da883b50f76eac461d3e0578f739
Namiken <namiken2525@yahoo.co.jp>
2017-01-13 01:45:45 +0900

    お金のメッセージを変更

--- src/lbn/money/galion/GalionEditReason.java
+++ src/lbn/money/galion/GalionEditReason.java
@@ -1,5 +1,10 @@
 package lbn.money.galion;
 
+import lbn.util.Message;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
 public enum GalionEditReason {
 	mob_drop(true),
 	penalty(true),
@@ -18,5 +23,32 @@ public enum GalionEditReason {
 
 	public boolean isPrintMessageLog() {
 		return isPrintMessageLog;
+	}
+
+	/**
+	 * お金の取得・消費のメッセージをPlayerに表示する
+	 * @param p
+	 * @param galions
+	 */
+	public void sendMessageLog(Player p, int galions) {
+		if (galions == 0) {
+			return;
+		}
+
+		if (!isPrintMessageLog()) {
+			return;
+		}
+
+		boolean isMinus = (galions < 0);
+		if (isMinus) {
+			if (this == consume_shop || this == consume_strength) {
+				Message.sendMessage(p, ChatColor.YELLOW + "{0} Galions 消費した", galions);
+			} else {
+				Message.sendMessage(p, ChatColor.YELLOW + "{0} Galions 失った", galions);
+			}
+		} else {
+			Message.sendMessage(p,  ChatColor.YELLOW + "{0} Galions 入手した", galions);
+		}
+
 	}
 }
