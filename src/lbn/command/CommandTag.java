4f0b60fb2d24409da0af78dc7b30cf64be423c0a
fav <monmon25565@gmail.com>
2017-04-26 03:51:55 +0900

    add help

--- src/lbn/command/CommandTag.java
+++ src/lbn/command/CommandTag.java
@@ -12,7 +12,17 @@ import lbn.nametag.TagConfig;
 public class CommandTag implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
 		if (args.length == 0) {
-			return false;
+			sender.sendMessage(ChatColor.DARK_GREEN + "■■================================================■■");
+			sender.sendMessage(ChatColor.AQUA + "/tag list");
+			sender.sendMessage(ChatColor.BOLD + "タグの一覧を表示します");
+			sender.sendMessage("");
+			sender.sendMessage(ChatColor.AQUA + "/tag settag <tag> <Player>" );
+			sender.sendMessage(ChatColor.BOLD + "↳タグをセットします");
+			sender.sendMessage("");
+			sender.sendMessage(ChatColor.AQUA + "/tag removetag <Player>");
+			sender.sendMessage(ChatColor.BOLD + "↳プレイヤーのタグを消します");
+			sender.sendMessage(ChatColor.DARK_GREEN + "■■================================================■■");
+			return true;
 		}
 		TagConfig.setNewTag(commandLabel);
 		Player online = Bukkit.getPlayerExact(args[0]);
