f03e7b8deeba060b171d70a9077c1413b1537937
fav <monmon25565@gmail.com>
2017-04-26 03:55:45 +0900

    static import

--- src/lbn/command/CommandTag.java
+++ src/lbn/command/CommandTag.java
@@ -1,7 +1,8 @@
 package lbn.command;
 
+import static org.bukkit.ChatColor.*;
+
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,16 +13,16 @@ import lbn.nametag.TagConfig;
 public class CommandTag implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
 		if (args.length == 0) {
-			sender.sendMessage(ChatColor.DARK_GREEN + "■■================================================■■");
-			sender.sendMessage(ChatColor.AQUA + "/tag list");
-			sender.sendMessage(ChatColor.BOLD + "    タグの一覧を表示します");
+			sender.sendMessage(DARK_GREEN + "■■================================================■■");
+			sender.sendMessage(AQUA + "/tag list");
+			sender.sendMessage(BOLD + "    タグの一覧を表示します");
 			sender.sendMessage("");
-			sender.sendMessage(ChatColor.AQUA + "/tag settag <tag> <Player>" );
-			sender.sendMessage(ChatColor.BOLD + "    タグをセットします");
+			sender.sendMessage(AQUA + "/tag settag <tag> <Player>" );
+			sender.sendMessage(BOLD + "    タグをセットします");
 			sender.sendMessage("");
-			sender.sendMessage(ChatColor.AQUA + "/tag removetag <Player>");
-			sender.sendMessage(ChatColor.BOLD + "    プレイヤーのタグを消します");
-			sender.sendMessage(ChatColor.DARK_GREEN + "■■================================================■■");
+			sender.sendMessage(AQUA + "/tag removetag <Player>");
+			sender.sendMessage(BOLD + "    プレイヤーのタグを消します");
+			sender.sendMessage(DARK_GREEN + "■■================================================■■");
 			return true;
 		}
 		TagConfig.setNewTag(commandLabel);
