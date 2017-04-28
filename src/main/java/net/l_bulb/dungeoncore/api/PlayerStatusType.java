a05841da98a5548435cda3c8badd57e25893feac
azure_crystal_ <stillcrystal@outlook.jp>
2017-04-28 20:43:49 +0900

    isPercentフィールドの名称をusingPercentageに変更し、private finalなフィールドへ変更

--- src/main/java/net/l_bulb/dungeoncore/api/PlayerStatusType.java
+++ src/main/java/net/l_bulb/dungeoncore/api/PlayerStatusType.java
@@ -4,20 +4,22 @@ public enum PlayerStatusType {
   ADD_SWORD_ATTACK, ADD_BOW_ATTACK, ADD_MAGIC_ATTACK, MULTIPLY_SWORD_ATTACK(true), MULTIPLY_BOW_ATTACK(true), MULTIPLY_MAGIC_ATTACK(
       true), MAX_MAGIC_POINT, MAX_HP;
 
-  boolean isPercent = false;
+  private final boolean usingPercentage;
 
   private PlayerStatusType(boolean isPercent) {
-    this.isPercent = isPercent;
+    this.usingPercentage = isPercent;
   }
 
-  private PlayerStatusType() {}
+  private PlayerStatusType() {
+    this(false);
+  }
 
   /**
    * 百分率ではなく割合で表しているならTRUE
-   * 
+   *
    * @return
    */
   public boolean isPercentage() {
-    return isPercent;
+    return usingPercentage;
   }
 }
