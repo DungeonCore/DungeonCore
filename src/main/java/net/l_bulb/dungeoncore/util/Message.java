94e7a2630ee6bff87cc45068cfaab460e167dc5e
azure_crystal_ <stillcrystal@outlook.jp>
2017-04-28 16:33:20 +0900

    無駄な引数を取るメソッドを非推奨に変更

--- src/main/java/net/l_bulb/dungeoncore/util/Message.java
+++ src/main/java/net/l_bulb/dungeoncore/util/Message.java
@@ -10,6 +10,7 @@ import net.l_bulb.dungeoncore.common.event.player.TheLowPlayerEvent;
 public class Message {
   static String[] sign = { "{0}", "{1}", "{2}", "{3}", "{4}", "{5}", "{6}", "{7}", "{8}", "{9}", "{10}", "{11}", "{12}", "{13}", "{14}" };
 
+  @Deprecated
   public static String getMessage(Player p, String message, Object... arg) {
     return getMessage(message, arg);
   }
