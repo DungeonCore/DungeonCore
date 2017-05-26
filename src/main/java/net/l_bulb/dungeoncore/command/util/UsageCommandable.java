24c1ea23b659f0288d795984d7532eede0603168
Namiken <namiken2525@yahoo.co.jp>
2017-05-26 21:02:10 +0900

    javadocを設定

--- src/main/java/net/l_bulb/dungeoncore/command/util/UsageCommandable.java
+++ src/main/java/net/l_bulb/dungeoncore/command/util/UsageCommandable.java
@@ -1,7 +1,17 @@
 package net.l_bulb.dungeoncore.command.util;
 
 public interface UsageCommandable {
+  /**
+   * コマンドの使い方を取得
+   * 
+   * @return
+   */
   String getUsage();
 
+  /**
+   * コマンドの説明を取得
+   * 
+   * @return
+   */
   String getDescription();
 }
