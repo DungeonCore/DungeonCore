b9360681a74618d9cbb6e09ce4650c0e51079dc8
azure_crystal_ <stillcrystal@outlook.jp>
2017-07-24 00:29:36 +0900

    パッチャーの可読性を改善させた
    Iteratorの代わりにStreamを使うようにしました。

--- src/main/java/net/l_bulb/dungeoncore/common/other/CitizenBugFixPatch.java
+++ src/main/java/net/l_bulb/dungeoncore/common/other/CitizenBugFixPatch.java
@@ -1,54 +1,41 @@
 package net.l_bulb.dungeoncore.common.other;
 
+import net.l_bulb.dungeoncore.dungeoncore.Main;
+import org.bukkit.Bukkit;
+
 import java.lang.reflect.Field;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
-
-import org.bukkit.scheduler.BukkitRunnable;
-
-import net.l_bulb.dungeoncore.dungeoncore.Main;
-
-import net.minecraft.server.v1_8_R1.EntityTypes;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CitizenBugFixPatch {
-  public static void doPatch() {
-    new BukkitRunnable() {
-      @Override
-      public void run() {
-        try {
-          Field[] declaredFields = EntityTypes.class.getDeclaredFields();
-          for (Field field : declaredFields) {
-            if (field.toGenericString().contains("EntityTypes.c")) {
-              field.setAccessible(true);
-              @SuppressWarnings("unchecked")
-              // EntityTypes.cを取得
-              Map<String, Class<?>> map = (Map<String, Class<?>>) field.get(null);
-              // 追加するMap
-              Map<String, Class<?>> addMap = new HashMap<>();
 
-              // １つずつ見て、もしKeyがClassならStringにする
-              Iterator<?> iterator = map.entrySet().iterator();
-              while (iterator.hasNext()) {
-                Entry<?, ?> next = (Entry<?, ?>) iterator.next();
-                // Classか確認
-                if (next.getKey() instanceof Class<?>) {
-                  // 一旦消す
-                  iterator.remove();
-                  // valueがnullでないなら値を入れる
-                  if (next.getValue() != null) {
-                    addMap.put(((Class<?>) next.getKey()).getSimpleName(), (Class<?>) next.getValue());
-                  }
-                }
-              }
-              map.putAll(addMap);
-            }
-          }
-        } catch (IllegalArgumentException | IllegalAccessException e) {
-          e.printStackTrace();
-        }
-      }
-    }.runTaskLater(Main.plugin, 20 * 5);
+  /**
+   * Removes abnormal keys from an internal field.
+   */
+  @SuppressWarnings("unchecked")
+  private static void applyPatch() {
+    final Logger logger = Logger.getLogger("DungeonCore");
+    try {
+      Field cField = Class.forName("net.minecraft.server.v1_8_R1.EntityTypes").getDeclaredField("c");
+      cField.setAccessible(true);
+      Map<String, Class<?>> c = (Map<String, Class<?>>) cField.get(null); // Static Field
+      // noinspection SuspiciousMethodCalls (for abnormal types)
+      new HashMap<Object, Class<?>>(c).entrySet().stream()
+          .filter(e -> e.getKey() instanceof Class<?>)
+          .peek(e -> c.remove(e.getKey()))
+          .filter(e -> e.getValue() != null)
+          .forEach(e -> c.put(((Class<?>) e.getKey()).getSimpleName(), e.getValue()));
+    } catch (ClassNotFoundException | NoSuchFieldException exception) {
+      logger.warning("このバージョンはサポートされていません。");
+      logger.log(Level.FINEST, "For Debug: ", exception);
+    } catch (IllegalArgumentException | IllegalAccessException exception) {
+      logger.log(Level.SEVERE, "パッチを適用できませんでした。", exception);
+    }
+  }
+
+  public static void doPatch() {
+    Bukkit.getScheduler().runTaskLater(Main.plugin, CitizenBugFixPatch::applyPatch, 20 * 5);
   }
 }
